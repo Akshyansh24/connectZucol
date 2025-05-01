061534badb88654c8a3187d32d6dad3aa32f89f0
Akshyansh24 <axkshs234@gmail.com>
2025-05-01 15:59:48 +0530

    strcuture update with proper section

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../components/layout/Header'
 import { Outlet } from 'react-router-dom'
+import Footer from '../components/layout/Footer'
 
 function HomeLayout() {
   return (
     <>
         <Header/>
         <Outlet/>
+        <Footer/>
     </>
   )
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import OurService from './ourServices/OurService'
+import WhyChooseUs from './chooseUs/WhyChooseUs'
+import CounterSection from './counter/CounterSection'
+import Certifications from './certification/Certifications'
+import Testimonial from './testimonial/Testimonial'
+import Banner from './banner/Banner'
 
 function Home() {
   return (
-    <div>Home</div>
+    <>
+    <Banner/>
+        <OurService/>
+        <WhyChooseUs/>
+        <CounterSection/>
+        <Certifications/>
+        <Testimonial/>
+    </>
   )
 }
 
